4c618cf3df0fd5dd84343c0cddd88686a94bb7a7
jwong38 <jwong38@mail.sfsu.edu>
2019-11-12 21:14:03 -0800

    Initial Commit

---
+++ CSC690-FinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CSC690-FinalProject
+//
+//  Created by Johnson Wong on 11/12/19.
+//  Copyright © 2019 Johnson Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
