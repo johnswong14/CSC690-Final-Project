4d81fb9b61f45edd0da542381a7fd280af1f0fc4
Johnson Wong <jwong38@mail.sfsu.edu>
2019-12-01 01:20:30 -0800

    added SwiftUI

---
+++ CSC690-Final-Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CSC690-Final-Project
+//
+//  Created by Johnson Wong on 12/1/19.
+//  Copyright © 2019 Johnson Wong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
